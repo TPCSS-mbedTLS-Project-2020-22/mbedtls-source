2f3613234cc32341c1a53f870c6d8f631b10405e
Vimal Patel <vimalm@iisc.ac.in>
2020-12-08 13:28:33 +0530

    Implement md2 with tests

---
+++ src/error/mod.rs
@@ -0,0 +1,9 @@
+/** 
+ * This file needs attention.
+ * 
+ * Currently it only defines one 
+ * error code which was needed by md2.c
+*/
+
+///This is a bug in the library
+pub const CORRUPTION_DETECTED:i16 = -0x006E;
