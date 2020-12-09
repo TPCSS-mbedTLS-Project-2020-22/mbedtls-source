8273e5b1a6dab6ce37656d5c263d63b6d4c38fff
Shashank Singh <cse.shashanksingh@gmail.com>
2020-12-09 21:20:51 +0530

    Init cargo library crate. Built the project structure

---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+mod ssl_tls;
---
+++ target/rls/debug/deps/mbed-40a05e7d4e76ba70.d
@@ -0,0 +1,6 @@
+/home/fragger/workspace/rust/mbedtls-source/target/rls/debug/deps/mbed-40a05e7d4e76ba70.rmeta: src/lib.rs src/ssl_tls.rs
+
+/home/fragger/workspace/rust/mbedtls-source/target/rls/debug/deps/mbed-40a05e7d4e76ba70.d: src/lib.rs src/ssl_tls.rs
+
+src/lib.rs:
+src/ssl_tls.rs:
