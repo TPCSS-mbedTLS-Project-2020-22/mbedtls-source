70875ca025214020ef6b8973cfca97b533433456
Ashish Kankal <ashish25895@gmail.com>
2021-01-01 17:18:57 +0530

    added data structures and function definitions

--- examples/camellia.rs
+++ examples/camellia.rs
@@ -1,4 +1,16 @@
 use mbed::cipher::camellia::*;
-fn main(){
+fn main() {
     test();
+    let keybits: u32 = 128;
+    let key = [4u8; 16];
+
+    let cipher = CamelliaContext::init(key, keybits);
+
+    let encrypted_value: i32 = cipher.encrypt(key);
+
+    println!("Encrypted Value of the text: {}", encrypted_value);
+
+    let decrypted_value: i32 = cipher.decrypt(key);
+
+    println!("Decrypted Value of the text: {}", decrypted_value);
 }
--- src/cipher/camellia.rs
+++ src/cipher/camellia.rs
@@ -1,4 +1,40 @@
+// Data structures
+#[derive(Debug, Clone)]
+pub struct CamelliaContext {
+    pub nr: i32,
+    pub rk: [u32; 68],
+}
 
-pub fn test(){
-    println!("Hello World from Camellia !");
+impl CamelliaContext {
+    pub fn init(key: [u8; 16], keybits: u32) -> CamelliaContext {
+        // Set nr
+        let nr = 4;
+        let rk = [0; 68];
+        // Schedule Keys
+        Self::key_schedule(&key);
+        CamelliaContext { nr, rk }
+    }
+    fn key_schedule(key: &[u8]) {
+        Self::camellia_feistel();
+    }
+
+    fn camellia_feistel() {}
+
+    pub fn encrypt(&self, text: [u8; 16]) -> i32 {
+        0
+    }
+    pub fn decrypt(&self, cipher_text: [u8; 16]) -> i32 {
+        0
+    }
+}
+
+// Constants
+pub const SIGMA_CHARS: [[u8; 8]; 6] = [[0u8; 8]; 6];
+
+pub const S_BOX: [[u32; 256]; 4] = [[0u32; 256]; 4];
+
+//Util Functions
+pub fn camellia_feistel() {}
+pub fn test() {
+    println!("====|| Camellia Cipher Algorithm ||====");
 }
