60954123ca8f1f765c9b612d80e68ee4b3c43b81
Shashank Singh <cse.shashanksingh@gmail.com>
2021-01-08 12:19:54 +0530

    Changed usages of tcp_ip to net_sockets

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ fn main() {
     let msg = (&args[3])[..].as_bytes();
 
     let proto = match &proto_str[..] {
-        "TCP" => tcp_ip::TLProtocol::TCP,
-        "UDP" => tcp_ip::TLProtocol::UDP,
+        "TCP" => net_sockets::TLProtocol::TCP,
+        "UDP" => net_sockets::TLProtocol::UDP,
         _ => panic!("Undefined Protocol"),
     };
 
@@ -19,33 +19,33 @@ fn main() {
     println!("\nStarting {} {}...\n", proto_str, mode);
 
     //Initialize context
-    let mut context = tcp_ip::MbedtlsNetContext::new(proto);
+    let mut context = net_sockets::MbedtlsNetContext::new(proto);
     // match context.protocol.unwrap() {
-    //     tcp_ip::TLProtocol::TCP => println!("TCP"),
-    //     tcp_ip::TLProtocol::UDP => println!("UDP")
+    //     net_sockets::TLProtocol::TCP => println!("TCP"),
+    //     net_sockets::TLProtocol::UDP => println!("UDP")
     // }
 
 
     if mode.eq_ignore_ascii_case("SERVER") {
 
         println!("Trying to open a {} socket", proto_str);
-        tcp_ip::mbedtls_net_bind(&mut context, "127.0.0.1", "4442", &proto);
+        net_sockets::mbedtls_net_bind(&mut context, "127.0.0.1", "4442", &proto);
 
         loop{
-            let mut context_client = tcp_ip::MbedtlsNetContext::new(proto);
-            tcp_ip::mbedtls_net_accept(&mut context, &mut context_client);
+            let mut context_client = net_sockets::MbedtlsNetContext::new(proto);
+            net_sockets::mbedtls_net_accept(&mut context, &mut context_client);
             
             let mut buf: [u8; 512] = [0; 512];
-            tcp_ip::mbedtls_net_recv(&mut context_client, &mut buf, 6); 
+            net_sockets::mbedtls_net_recv(&mut context_client, &mut buf, 6); 
             println!("Received message : {}", String::from_utf8_lossy(&buf));
         }
     } 
     else if mode.eq_ignore_ascii_case("CLIENT") {
         println!("Trying to connect to the open socket");
-        tcp_ip::mbedtls_net_connect(&mut context, "127.0.0.1", "4442", &proto);
+        net_sockets::mbedtls_net_connect(&mut context, "127.0.0.1", "4442", &proto);
 
         println!("Sending message to server : {}", &args[3]);
-        tcp_ip::mbedtls_net_send(&mut context, msg);
+        net_sockets::mbedtls_net_send(&mut context, msg);
     }
     
 
