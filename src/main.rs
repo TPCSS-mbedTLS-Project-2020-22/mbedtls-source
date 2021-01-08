fabf42c5aa1765a2851aef483d5c2cf4e92340bf
Shashank Singh <cse.shashanksingh@gmail.com>
2021-01-08 12:31:31 +0530

    Added bidirectional message exchange for test implementation

--- src/main.rs
+++ src/main.rs
@@ -36,20 +36,29 @@ fn main() {
             net_sockets::mbedtls_net_accept(&mut context, &mut context_client);
             
             let mut buf: [u8; 512] = [0; 512];
+
             net_sockets::mbedtls_net_recv(&mut context_client, &mut buf, 6); 
             println!("Received message : {}", String::from_utf8_lossy(&buf));
+
+            net_sockets::mbedtls_net_send(&mut context_client, msg); 
+            println!("Sent response message to client : {:}", String::from_utf8_lossy(&msg));
         }
     } 
     else if mode.eq_ignore_ascii_case("CLIENT") {
         println!("Trying to connect to the open socket");
         net_sockets::mbedtls_net_connect(&mut context, "127.0.0.1", "4442", &proto);
 
-        println!("Sending message to server : {}", &args[3]);
+        let mut buf: [u8; 512] = [0; 512];
+
+        println!("Sending message to server : {}", String::from_utf8_lossy(&msg));
         net_sockets::mbedtls_net_send(&mut context, msg);
+
+        net_sockets::mbedtls_net_recv(&mut context, &mut buf, 7); 
+        println!("Received response from server : {}", String::from_utf8_lossy(&buf));
     }
     
 
-    println!("Press enter to end the program : ");
-    let mut line = String::new();
-    let b1 = std::io::stdin().read_line(&mut line).unwrap();
+    // println!("Press enter to end the program : ");
+    // let mut line = String::new();
+    // let b1 = std::io::stdin().read_line(&mut line).unwrap();
 }
