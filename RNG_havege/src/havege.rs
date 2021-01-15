0443dbe4425fcb9f17942d6689b2da00356d537c
Shubham Dobhal <shubhamdobhal10@gmail.com>
2021-01-15 11:17:24 +0530

    havege random funtion added to havege submodule of RNG

--- RNG_havege/src/havege.rs
+++ RNG_havege/src/havege.rs
@@ -1,9 +1,13 @@
 use std::mem;
+pub const MBEDTLS_HAVEGE_COLLECT_SIZE:usize =  1024;
+/*
+ * HAVEGE state structure
+ */
 pub struct mbedtls_havege_state{
 	pt1:u32,
 	pt2:u32,
 	offset:[i32;2],
-	pool:[i32;1024],	
+	pool:[i32;MBEDTLS_HAVEGE_COLLECT_SIZE],	
 	walk:[i32;8192]
 }
 impl mbedtls_havege_state{
@@ -11,16 +15,51 @@ impl mbedtls_havege_state{
             mbedtls_havege_state{ pt1: pt1, pt2: pt2,offset:offset,pool:pool,walk:walk }
     }
 }
+
+
+/*
+ * Entropy gathering function
+ */
+pub fn havege_fill(hs:&mut mbedtls_havege_state){
+    hs.offset[0]=0;
+    hs.offset[1] = MBEDTLS_HAVEGE_COLLECT_SIZE as i32 / 2;
+    println!("{}",hs.pt1);
+}
+
+/*
+ * HAVEGE initialization
+ */
 pub fn initialise(hs:&mut mbedtls_havege_state){
     hs.pt1=0;
     hs.pt2=0;
     hs.offset[0] = 0;
     hs.offset[1] = 0;
-    for i in 0..1024{
+    for i in 0..MBEDTLS_HAVEGE_COLLECT_SIZE{
         hs.pool[i] = 0;
     }
     for i in 0..8192{
         hs.walk[i]=0;
     }
-    //havege_fill(hs);
+    havege_fill(hs);
+}
+
+/*
+ * HAVEGE rand function
+ */
+pub fn havege_random(hs:&mut mbedtls_havege_state, buf:&mut String, len: usize)->i32{
+    let mut val:usize=0;
+    let mut use_len:usize=0;
+    while use_len < len {
+        if hs.offset[1] >= MBEDTLS_HAVEGE_COLLECT_SIZE as i32{
+            havege_fill( hs );
+        }
+        val  = hs.pool[hs.offset[0] as usize] as usize;
+        val = val ^ hs.pool[hs.offset[1] as usize] as usize;
+        //println!("{}",use_len);
+        hs.offset[0]+=1;
+        hs.offset[1]+=1;
+        buf.push_str(& val.to_string());
+        use_len = use_len + mem::size_of_val(&val);
+    }
+    return 0;
 }
--- RNG_havege/src/main.rs
+++ RNG_havege/src/main.rs
@@ -1,3 +1,5 @@
+//HAVEGE: HArdware Volatile Entropy Gathering and Expansion
+// This module generate random data into a file (data.txt in this case)
 mod havege;
 use std::fs::File;
 use std::io::prelude::*;
@@ -5,7 +7,7 @@ use std::path::Path;
 use std::mem;
 
 fn main() {
-    let len:usize = 1024;
+    let len:usize = havege::MBEDTLS_HAVEGE_COLLECT_SIZE;
     let mut hs = havege::mbedtls_havege_state::new(0,0,[0;2],[0;1024],[0;8192]);
     
     let path = Path::new("data.txt");
@@ -17,7 +19,7 @@ fn main() {
     havege::initialise(&mut hs);
     for i in 1..768{
         let mut buff = String::from("hello");
-        //havege::havege_random(&mut hs,&mut buff,len);
+        havege::havege_random(&mut hs,&mut buff,len);
         
         match file.write_all(buff.as_bytes()) {
             Err(why) => panic!("couldn't write to {}: {}", display, why),
