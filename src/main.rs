b929d4d16486dd1d9e10f8df77eb368ff23f6005
Abhishek Singh <abhi06011997@gmail.com>
2021-01-23 00:14:16 +0530

    final commit

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-mod test;
 mod bignum_dep;
 mod diffie_hellman;
 
-use test::check;
 use bignum_dep::bignum1;
 use diffie_hellman::dhm;
-// use test::check::ch;
 
 fn main()
 {
-    test::check::print();
-    check::checktwo();
     bignum1::print();
     dhm::print();
 }
